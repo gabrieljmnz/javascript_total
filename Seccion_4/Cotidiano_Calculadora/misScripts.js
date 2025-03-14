aedeb433c8701d4a40c35530ee94e8e574d52237
gabrieljmnz <angelgabrielvargas313@gmail.com>
2025-03-14 23:01:18 +0100

    Cotidiano calculadora, Falta terminar

--- Seccion_4/Cotidiano_Calculadora/misScripts.js
+++ Seccion_4/Cotidiano_Calculadora/misScripts.js
@@ -1,4 +1,12 @@
-let 
+var elementoDato1 = document.getElementById("dato1");
+var elementoDato2 = document.getElementById("dato2");
+var elementoResultado = document.getElementById("resultado")
 function suma() {
-
+    sumaResp = parseInt(elementoDato1) + parseInt(elementoDato2);
+}
+function resta() {
+    restaResp = parseInt(elementoDato1) - parseInt(elementoDato2);
+}
+function multiplicacion() {
+    
 }
